610bf2ff1e779dee5d5e35f6c59d36ba1b520ad7
ShuoY1n <jackshuoyi@gmail.com>
2026-01-13 16:19:22 -0500

    added minstack

---
+++ medium/minimum_stack.py
@@ -0,0 +1,52 @@
+"""
+Minimum Stack
+Problem: https://neetcode.io/problems/minimum-stack/history
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+- MinStack() initializes the stack object.
+- void push(int val) pushes the element val onto the stack.
+- void pop() removes the element on the top of the stack.
+- int top() gets the top element of the stack.
+- int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+"""
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.m = []
+
+        
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.m.append(val)
+
+        self.stack.append(val)
+
+        if val <= self.m[-1]:
+            self.m.append(val)
+
+        
+        
+
+    def pop(self) -> None:
+        if self.m and self.m[-1] == self.stack[-1]:
+            self.m.pop()
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if self.m:
+            return self.m[-1]
+        else:
+            return None
+
